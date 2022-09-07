5ca114502b5ec991a64e9a5675c36d4c00cb8e29
Karoline Sarmento <100390180+mariakarolinesarmento@users.noreply.github.com>
2022-09-06 21:39:23 -0300

    Add files via upload

--- 3.3 Migracao de dados com Flyway - Spring Boot 2.7.0/algamoney-api/src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
+++ 3.3 Migracao de dados com Flyway - Spring Boot 2.7.0/algamoney-api/src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
@@ -3,10 +3,12 @@ package com.example.algamoney.api.resource;
 import com.example.algamoney.api.model.Categoria;
 import com.example.algamoney.api.repository.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -20,4 +22,17 @@ public class CategoriaResource {
     public List<Categoria> listar(){
         return categoriaRepository.findAll();
     }
+
+     @PostMapping
+     public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response){
+        Categoria categoriaSalva = categoriaRepository.save(categoria);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+                .buildAndExpand(categoriaSalva.getCodigo()).toUri();
+        response.setHeader("Location", uri.toASCIIString());
+        return ResponseEntity.created(uri).body(categoriaSalva);
+     }
+     @GetMapping("/{codigo}")
+     public Categoria buscarPeloCodigo(@PathVariable Long codigo){
+        return categoriaRepository.findById(codigo).orElse(null);
+     }
 }
